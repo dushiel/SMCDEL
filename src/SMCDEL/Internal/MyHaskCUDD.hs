350c42b69b5dd90a7b892a645db818e00044c77b
daniel <mannetje_miedema@hotmail.com>
2020-09-10 14:53:49 +0200

    reformatted the zdd and bdd functions into a Dd class

--- src/SMCDEL/Internal/MyHaskCUDD.hs
+++ src/SMCDEL/Internal/MyHaskCUDD.hs
@@ -8,19 +8,21 @@ module SMCDEL.Internal.MyHaskCUDD (
   exists, forall, forallSet, existsSet,
   restrict, restrictSet,
   ifthenelse,
-  gfp,
+  gfp, 
   -- * Zdd functionalities
-  topZ, varZ, differenceZ, botZ, restrictZ, restrictSetZ, portVars, createZddFromBdd,
+  gfpZ, writeToDot, printDdInfo, differenceZ, portVars, initZddVarsWithInt, topZ, varZ, botZ,
+  createZddFromBdd
+  {-topZ, varZ, differenceZ, botZ, restrictZ, restrictSetZ, portVars, createZddFromBdd,
   negZ, conZ, disZ, impZ, disSetZ, conSetZ, xorZ, xorSetZ,
   existsZ, existsSetZ, forallZ, forallSetZ, equZ, ifthenelseZ, gfpZ, initZddVarsWithInt,
   -- * New helper functions
-  writeZdd, writeBdd, printZddInfo, printBddInfo
+  writeZdd, writeBdd, printZddInfo, printBddInfo-}
 ) where
 
 import qualified Cudd.Cudd
 import System.IO.Unsafe
 
-{-}
+
 manager :: Cudd.Cudd.DdManager
 manager = Cudd.Cudd.cuddInit
 
@@ -41,73 +43,91 @@ varZ :: Int -> Zdd
 varZ i = ToZdd (Cudd.Cudd.cuddZddIthVar manager i)
 
 -------------------------------------------------------------------------------------------
-class DdF a 
+class DdF a where
+  neg :: a -> a
+  con :: a -> a -> a
+  dis :: a -> a -> a
+  xor :: a -> a -> a
+  equ :: a -> a -> a
+  imp :: a -> a -> a
+  exists :: Int -> a -> a
+  forall :: Int -> a -> a
+  existsSet :: [Int] -> a -> a
+  forallSet :: [Int] -> a -> a
+  conSet :: [a] -> a
+  disSet :: [a] -> a
+  xorSet :: [a] -> a
+  ifthenelse :: a -> a -> a -> a
+  restrict :: a -> (Int,Bool) -> a
+  restrictSet :: a -> [(Int,Bool)] -> a
+  writeToDot :: a -> String -> IO()
+  printDdInfo :: a -> String -> IO()
+
+
 --
-instance DdF Bdd -> Bdd where
+instance DdF Cudd.Cudd.DdNode where
   neg =  Cudd.Cudd.cuddNot manager
-
-instance DdF Zdd -> Zdd where
-  neg = topZ `differenceZ` z
-
---
-instance DdF Bdd -> Bdd -> Bdd where
   con =  Cudd.Cudd.cuddBddAnd manager
   dis =  Cudd.Cudd.cuddBddOr manager
   xor =  Cudd.Cudd.cuddBddXor manager
   equ a b = con (imp a b) (imp b a)
   imp b1 b2 =  Cudd.Cudd.cuddBddIte manager b1 b2 top
-
-instance DdF Zdd -> Zdd -> Zdd where
-  con (ToZdd z1) (ToZdd z2) = ToZdd (Cudd.Cudd.cuddZddIntersect manager z1 z2)
-  dis (ToZdd z1) (ToZdd z2) = ToZdd (Cudd.Cudd.cuddZddUnion manager z1 z2)
-  xor z1 z2 =  a `conZ` b where
-    a = differenceZ z1 z2 
-    b = differenceZ z2 z1
-  equ a b = con (imp a b) (imp b a)
-  impZ (ToZdd z1) (ToZdd z2) = ToZdd $ Cudd.Cudd.cuddZddITE manager z1 z1 t where
-    ToZdd t = topZ
-
---
-instance DdF Int -> Bdd -> Bdd where
+  ifthenelse =  Cudd.Cudd.cuddBddIte manager
   exists n b =  Cudd.Cudd.cuddBddExistAbstract manager b ( Cudd.Cudd.cuddIndicesToCube manager [n])
   forall n b =  Cudd.Cudd.cuddBddUnivAbstract manager b ( Cudd.Cudd.cuddIndicesToCube manager [n])
+  restrict b (n,bit) =  Cudd.Cudd.cuddBddLICompaction manager b res where
+    res = if bit then var n else neg (var n)
 
-instance DdF Int -> Zdd -> Zdd where
-  exists n (ToZdd zdd) =  x `dis` y where
-    x = ToZdd $ Cudd.Cudd.cuddZddSub0 manager zdd n
-    y = ToZdd $ Cudd.Cudd.cuddZddSub1 manager zdd n
-  forall n (ToZdd zdd) = ifthenelse (varZ n) x y where
-   x = ToZdd $ Cudd.Cudd.cuddZddSub1 manager zdd n
-   y = ToZdd $ Cudd.Cudd.cuddZddSub0 manager zdd n 
-  
-  
---
-instance DdF [Int] -> Bdd -> Bdd where
+  --set versions
   existsSet [] b = b
   existsSet ns b =  Cudd.Cudd.cuddBddExistAbstract manager b ( Cudd.Cudd.cuddIndicesToCube manager ns)
   forallSet [] b = b
   forallSet ns b =  Cudd.Cudd.cuddBddUnivAbstract manager b ( Cudd.Cudd.cuddIndicesToCube manager ns)
-
-instance DdF [Int] -> Zdd -> Zdd where
-  forallSet [] _ = error "empty UniversalVar list"
-  forallSet [n] z = forall n z
-  forallSet (n:ns) z = x `dis` forallSet ns x where 
-    x = forall n z
-  existsSetZ [] _ = error "empty ExistsVar list"
-  existsSetZ [n] z = exists n z
-  existsSetZ (n:ns) z = x `con` forallSet ns x where 
-    x = forall n z
-  
---
-instance DdF [Bdd]-> Bdd where
   conSet [] = top
   conSet (b:bs) = foldl con b bs
   disSet [] = bot
   disSet (b:bs) = foldl dis b bs
   xorSet [] = bot
   xorSet (b:bs) = foldl xor b bs
+  restrictSet b bits =  Cudd.Cudd.cuddBddLICompaction manager b res where
+    res = conSet $ map (\(n,bit) -> if bit then var n else neg (var n)) bits
+  
 
-instance DdF [Zdd]-> Zdd where
+  --helper functions
+  printDdInfo bdd str = do
+    putStrLn str
+    Cudd.Cudd.cuddPrintDdInfo manager bdd
+  writeToDot bdd f = Cudd.Cudd.cuddBddToDot manager bdd f
+
+
+instance DdF Zdd where
+  neg z = topZ `differenceZ` z
+  con (ToZdd z1) (ToZdd z2) = ToZdd (Cudd.Cudd.cuddZddIntersect manager z1 z2)
+  dis (ToZdd z1) (ToZdd z2) = ToZdd (Cudd.Cudd.cuddZddUnion manager z1 z2)
+  xor z1 z2 =  a `con` b where
+    a = differenceZ z1 z2 
+    b = differenceZ z2 z1
+  equ a b = con (imp a b) (imp b a)
+  imp (ToZdd z1) (ToZdd z2) = ToZdd $ Cudd.Cudd.cuddZddITE manager z1 z2 t where
+    ToZdd t = topZ
+  ifthenelse (ToZdd x) (ToZdd y) (ToZdd z) = ToZdd (Cudd.Cudd.cuddZddITE manager x y z)
+  exists n zdd =  neg $ forall n $ neg $ zdd
+  forall n (ToZdd zdd) = x `dis` y where
+    x = ToZdd $ Cudd.Cudd.cuddZddSub0 manager zdd n
+    y = ToZdd $ Cudd.Cudd.cuddZddChange manager (Cudd.Cudd.cuddZddSub1 manager zdd n) n
+  restrict (ToZdd zdd) (n,bit) =  ToZdd $ if bit 
+    then Cudd.Cudd.cuddZddChange manager (Cudd.Cudd.cuddZddSub1 manager zdd n) n
+  else Cudd.Cudd.cuddZddSub0 manager zdd n
+
+  --Set versions
+  forallSet [] _ = error "empty UniversalVar list"
+  forallSet [n] z = forall n z
+  forallSet (n:ns) z = x `dis` forallSet ns x where 
+    x = forall n z
+  existsSet [] _ = error "empty ExistsVar list"
+  existsSet [n] z = exists n z
+  existsSet (n:ns) z = x `con` forallSet ns x where 
+    x = forall n z
   conSet [] = error "empty AND list"
   conSet [z] = z
   conSet (z:zs) = foldl con z zs
@@ -117,14 +137,20 @@ instance DdF [Zdd]-> Zdd where
   xorSet [] = error "empty XOR list"
   xorSet [z] = z
   xorSet (z:zs) = foldl xor z zs
+  restrictSet _ [] = error "restricting with empty list"
+  restrictSet zdd (n : []) = restrict zdd n
+  restrictSet zdd (n : ns) = restrictSet (restrict zdd n) ns
+  
 
---
-instance DdF Bdd -> Bdd -> Bdd -> Bdd where
-  ifthenelse =  Cudd.Cudd.cuddBddIte manager
+  --helper functions
+  printDdInfo (ToZdd zdd) str = do
+    putStrLn str
+    Cudd.Cudd.cuddPrintDdInfo manager zdd
+  writeToDot (ToZdd zdd) f = Cudd.Cudd.cuddZddToDot manager zdd f
 
-instance DdF Zdd -> Zdd -> Zdd -> Zdd where
-  ifthenelseZ (ToZdd x) (ToZdd y) (ToZdd z) = ToZdd (Cudd.Cudd.cuddZddITE manager x y z)
-----------------------------------------------------------------------------------------------}
+---------------------------------------------------------------------------------------------
+
+{-}
 type Bdd =  Cudd.Cudd.DdNode
 
 manager :: Cudd.Cudd.DdManager
@@ -184,7 +210,7 @@ disSet (b:bs) = foldl dis b bs
 
 xorSet :: [Bdd] -> Bdd
 xorSet [] = bot
-xorSet (b:bs) = foldl xor b bs
+xorSet (b:bs) = foldl xor b bs-}
 
 gfp :: (Bdd -> Bdd) -> Bdd
 gfp operator = gfpLoop top where
@@ -193,7 +219,7 @@ gfp operator = gfpLoop top where
     if current == operator current
       then current
       else gfpLoop (operator current)
-
+{-}
 restrict :: Bdd -> (Int,Bool) -> Bdd
 restrict b (n,bit) =  Cudd.Cudd.cuddBddLICompaction manager b res where
   res = if bit then var n else neg (var n)
@@ -278,7 +304,7 @@ forallSetZ [] _ = error "empty UniversalVar list"
 forallSetZ [n] z = forallZ n z
 forallSetZ (n:ns) z = x `conZ` forallSetZ ns x where 
   x = forallZ n z
-
+-}
 gfpZ :: (Zdd -> Zdd) -> Zdd
 gfpZ operator = gfpLoop topZ where
   gfpLoop :: Zdd -> Zdd
@@ -287,12 +313,17 @@ gfpZ operator = gfpLoop topZ where
       then current
       else gfpLoop (operator current)
 
-ifthenelseZ :: Zdd -> Zdd -> Zdd -> Zdd
-ifthenelseZ (ToZdd x) (ToZdd y) (ToZdd z) = ToZdd (Cudd.Cudd.cuddZddITE manager x y z)
 
 differenceZ :: Zdd -> Zdd -> Zdd
 differenceZ (ToZdd x) (ToZdd y) = ToZdd (Cudd.Cudd.cuddZddDiff manager x y)
 
+
+initZddVarsWithInt :: [Int] -> Zdd
+initZddVarsWithInt [] = error "initialising empty vocabulary list of zdd vars"
+initZddVarsWithInt (n: []) = varZ n 
+initZddVarsWithInt (n: ns) = varZ n `dis` initZddVarsWithInt ns
+
+{-}
 restrictZ :: Zdd -> (Int,Bool) -> Zdd
 restrictZ (ToZdd zdd) (n,bit) =  ToZdd $ if bit 
   then Cudd.Cudd.cuddZddSub1 manager zdd n 
@@ -303,10 +334,8 @@ restrictSetZ _ [] = error "restricting with empty list"
 restrictSetZ zdd (n : []) = restrictZ zdd n
 restrictSetZ zdd (n : ns) = restrictSetZ (restrictZ zdd n) ns
 
-initZddVarsWithInt :: [Int] -> Zdd
-initZddVarsWithInt [] = error "initialising empty vocabulary list of zdd vars"
-initZddVarsWithInt (n: []) = varZ n 
-initZddVarsWithInt (n: ns) = varZ n `disZ` initZddVarsWithInt ns
+ifthenelseZ :: Zdd -> Zdd -> Zdd -> Zdd
+ifthenelseZ (ToZdd x) (ToZdd y) (ToZdd z) = ToZdd (Cudd.Cudd.cuddZddITE manager x y z)
 
 ---helper functions placed here for now
 
@@ -325,6 +354,7 @@ writeBdd bdd f = Cudd.Cudd.cuddBddToDot manager bdd f
 
 writeZdd:: Zdd -> String -> IO()
 writeZdd (ToZdd zdd) f = Cudd.Cudd.cuddZddToDot manager zdd f
+-}
 
 createZddFromBdd :: Bdd -> Zdd
 createZddFromBdd bdd = (ToZdd (Cudd.Cudd.cuddZddPortFromBdd manager bdd))
--- src/SMCDEL/Symbolic/S5_CUDD.hs
+++ src/SMCDEL/Symbolic/S5_CUDD.hs
@@ -37,7 +37,7 @@ pubAnnounce :: KnowStruct -> Form -> KnowStruct
 pubAnnounce kns@(KnS props lawbdd obs) psi = KnS props newlawbdd obs where
   newlawbdd = con lawbdd (bddOf kns psi)
 pubAnnounce kns@(KnSZ props lawzdd obs) psi = KnSZ props newlawzdd obs where
-  newlawzdd = conZ lawzdd (zddOf kns psi)
+  newlawzdd = con lawzdd (zddOf kns psi)
 
 announce :: KnowStruct -> [Agent] -> Form -> KnowStruct
 announce kns@(KnS props lawbdd obs) ags psi = KnS newprops newlawbdd newobs where
@@ -48,7 +48,7 @@ announce kns@(KnS props lawbdd obs) ags psi = KnS newprops newlawbdd newobs wher
 announce kns@(KnSZ props lawzdd obs) ags psi = KnSZ newprops newlawzdd newobs where
   proppsi@(P k) = freshp props
   newprops  = proppsi:props
-  newlawzdd = conZ lawzdd (equZ (varZ k) (zddOf kns psi))
+  newlawzdd = con lawzdd (equ (varZ k) (zddOf kns psi))
   newobs    = [(i, apply obs i ++ [proppsi | i `elem` ags]) | i <- map fst obs]
 
 bddOf :: KnowStruct -> Form -> Bdd
@@ -106,7 +106,124 @@ validViaBdd kns@(KnS _ lawbdd _) f = top == lawbdd `imp` bddOf kns f
 
 -- ZDD stuff (also see data type declarations above)
 
+
+convertTest :: KnowStruct -> Form -> Bool
+convertTest kns@(KnS _ law _) query = unsafePerformIO $ do
+  let q = createZddFromBdd (bddOf kns query)
+  let l = createZddFromBdd law
+  let r = l `imp` q
+  --printZddInfo q "convertionn result"
+  return (r == topZ)
+
+initZddVars :: [Int] -> IO()
+initZddVars vocab = do
+  let v = initZddVarsWithInt vocab
+  printDdInfo v "init!"
+  return ()
+
+
+-------------- building
+
+
 boolZddOf :: Form -> Zdd
+boolZddOf Top           = topZ
+boolZddOf Bot           = botZ
+boolZddOf (PrpF (P n))  = varZ n
+boolZddOf (Neg form)    = neg (boolZddOf form)
+boolZddOf (Conj forms)  = conSet $ map boolZddOf forms
+boolZddOf (Disj forms)  = disSet $ map boolZddOf forms
+boolZddOf (Impl f g)    = imp (boolZddOf f) (boolZddOf g)
+boolZddOf (Equi f g)    = equ (boolZddOf f) (boolZddOf g)
+boolZddOf (Forall ps f) = boolZddOf (foldl singleForall f ps) where
+  singleForall g p = Conj [ substit p Top g, substit p Bot g ] --Why this way???
+boolZddOf (Exists ps f) = boolZddOf (foldl singleExists f ps) where
+  singleExists g p = Disj [ substit p Top g, substit p Bot g ]
+boolZddOf _             = error "boolZddOf failed: Not a boolean formula."
+
+zddOf :: KnowStruct -> Form -> Zdd
+zddOf _   Top           = topZ
+zddOf _   Bot           = botZ
+zddOf _   (PrpF (P n))  = varZ n
+zddOf kns (Neg form) = neg (zddOf kns form)
+
+zddOf kns (Conj forms)  = conSet $ map (zddOf kns) forms
+zddOf kns (Disj forms)  = disSet $ map (zddOf kns) forms
+zddOf kns (Xor  forms)  = xorSet $ map (zddOf kns) forms
+
+zddOf kns (Impl f g)    = imp (zddOf kns f) (zddOf kns g)
+zddOf kns (Equi f g)    = equ (zddOf kns f) (zddOf kns g)
+
+zddOf kns (Forall ps f) = forallSet (map fromEnum ps) (zddOf kns f)
+zddOf kns (Exists ps f) = existsSet (map fromEnum ps) (zddOf kns f)
+
+zddOf kns@(KnSZ allprops lawzdd obs) (K i form) =
+  forallSet otherps (imp lawzdd (zddOf kns form)) where
+    otherps = map (\(P n) -> n) $ allprops \\ apply obs i
+
+zddOf kns@(KnSZ allprops lawzdd obs) (Kw i form) =
+  disSet [ forallSet otherps (imp lawzdd (zddOf kns f)) | f <- [form, Neg form] ] where
+    otherps = map (\(P n) -> n) $ allprops \\ apply obs i
+
+zddOf kns@(KnSZ allprops lawzdd obs) (Ck ags form) = gfpZ lambda where
+  lambda z = conSet $ zddOf kns form : [ forallSet (otherps i) (imp lawzdd z) | i <- ags ]
+  otherps i = map (\(P n) -> n) $ allprops \\ apply obs i
+
+zddOf kns (Ckw ags form) = dis (zddOf kns (Ck ags form)) (zddOf kns (Ck ags (Neg form)))
+
+zddOf kns@(KnSZ props _ _) (Announce ags form1 form2) =
+  imp (zddOf kns form1) (restrict zdd2 (k,True)) where
+    zdd2  = zddOf (announce kns ags form1) form2
+    (P k) = freshp props
+
+zddOf kns@(KnSZ props _ _) (AnnounceW ags form1 form2) =
+  ifthenelse (zddOf kns form1) zdd2a zdd2b where
+    zdd2a = restrict (zddOf (announce kns ags form1) form2) (k,True)
+    zdd2b = restrict (zddOf (announce kns ags form1) form2) (k,False)
+    (P k) = freshp props
+
+zddOf kns (PubAnnounce form1 form2) = imp (zddOf kns form1) newform2 where
+    newform2 = zddOf (pubAnnounce kns form1) form2
+
+zddOf kns (PubAnnounceW form1 form2) =
+  ifthenelse (zddOf kns form1) newform2a newform2b where
+    newform2a = zddOf (pubAnnounce kns form1) form2
+    newform2b = zddOf (pubAnnounce kns (Neg form1)) form2
+
+zddOf _ (Dia _ _) = error "Dynamic operators are not implemented for CUDD."
+
+--------------querying
+
+validViaZdd :: KnowStruct -> Form -> Bool
+validViaZdd kns@(KnSZ _ lawzdd _) f = unsafePerformIO $! do 
+  let a = (exists 2 (var 3)) `imp` var 3
+  let b = (exists 2 (varZ 3)) `imp` varZ 3
+  let c = (varZ 1 `con` botZ) `imp` (varZ 1 `dis` topZ)
+
+  let x = neg (forall 2 (neg $ var 3)) `imp` (var 3)
+  let y = neg (forall 2 (neg $ varZ 3)) `imp` (varZ 3)
+
+  putStrLn ("basic check of zdd, (1&bot) -> (1|top): " ++ show (c == topZ))
+  --printZddInfo c "basic check"
+  putStrLn ("exists zdd equal to bdd, on (E2(3) -> 3): " ++ show ((a == top) == (b == topZ)))
+  putStrLn ("forall zdd equal to bdd: on (A2(3) -> 3)" ++ show ((x == top) == (y == topZ)))
+  putStrLn ("see validViaZdd in S5_CUDD.hs for precise implementation.\n")
+  --putStrLn ("bdd exists and forall the same: " ++ show (a == x) ++ ", zdd exists and forall:" ++ show(b == y))
+  --printZddInfo ((negZ $ varZ 3) `impZ` (varZ 1)) "~3 ->1"--`impZ` varZ 1) "~3 -> 1"
+   
+  let z = topZ == lawzdd `imp` (zddOf kns f) 
+  return z
+
+evalViaZdd :: KnowScene -> Form -> Bool
+evalViaZdd (kns@(KnSZ allprops _ obs),s) f = bool where
+  bool | z==topZ = True
+       | z==botZ = False
+       | otherwise = error ("evalViaZdd failed: ZDD leftover:\n" ++ show z)
+  z    = restrictSet (zddOf kns f) list
+  list = [ (n, P n `elem` s) | (P n) <- allprops ]
+
+
+
+{-boolZddOf :: Form -> Zdd
 boolZddOf Top           = topZ
 boolZddOf Bot           = botZ
 boolZddOf (PrpF (P n))  = varZ n
@@ -191,38 +308,4 @@ validViaZdd kns@(KnSZ _ lawzdd _) f = unsafePerformIO $! do
    
   let z = topZ == lawzdd `impZ` (zddOf kns f) 
   return z
-
-convertTest :: KnowStruct -> Form -> Bool
-convertTest kns@(KnS _ law _) query = unsafePerformIO $ do
-  let q = createZddFromBdd (bddOf kns query)
-  let l = createZddFromBdd law
-  let r = l `impZ` q
-  --printZddInfo q "convertionn result"
-  return (r == topZ)
-
-evalViaZdd :: KnowScene -> Form -> Bool
-evalViaZdd (kns@(KnSZ allprops _ obs),s) f = bool where
-  bool | z==topZ = True
-       | z==botZ = False
-       | otherwise = error ("evalViaZdd failed: ZDD leftover:\n" ++ show z)
-  z    = restrictSetZ (zddOf kns f) list
-  list = [ (n, P n `elem` s) | (P n) <- allprops ]
-
-
-initZddVars :: [Int] -> IO()
-initZddVars vocab = do
-  let v = initZddVarsWithInt vocab
-  printZddInfo v "init!"
-  --portVars
-  return ()
-
-prpToInt :: [Prp] -> [Int]
-prpToInt vocab = map (\(P n) -> n) vocab
-
---dot print
-
-dotPrintZ :: Zdd -> IO()
-dotPrintZ b = writeZdd b "hello_zdd_graph"
-
-dotPrint :: Bdd -> IO()
-dotPrint b = writeBdd b "hello_bdd_graph"
+-}
